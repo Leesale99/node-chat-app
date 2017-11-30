165944625234a992b9ce97d092c8925c1f15a473
aleks_rdvn <aleks.rdvn@gmail.com>
2017-11-30 16:11:19 +0100

    Add geolocation support via geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,5 +11,14 @@ describe('generateMessage', () => {
     expect(message).toHaveProperty('from', 'Admin');
     expect(message).toHaveProperty('text', 'Hello');
     expect(message.createdAt).toEqual(expect.any(Number));
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const message = generateLocationMessage('Admin', '1', '2');
+
+    expect(message.from).toMatch('Admin');
+    expect(message.url).toMatch('https://www.google.com/maps?q=1,2');
   });
 });
